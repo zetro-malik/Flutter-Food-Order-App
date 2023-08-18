fa8fad4701b7e152554f4679eb7b87d8af430260
zetro-malik <zeeshanimsb@gmail.com>
2023-08-18 12:24:26 +0500

    add products in searchViewModel

--- lib/screens/search_food/search_food_viewmodel.dart
+++ lib/screens/search_food/search_food_viewmodel.dart
@@ -1,12 +1,25 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:food_order_ui/screens/menu/product_model.dart';
 import 'package:get/get.dart';
 
 class SearchFoodViewModel extends GetxController{
 
   RxInt selectedTab = 0.obs;
   double filterRating = 0;
+
+
+
+  List<Product> products = [
+    Product('asset/images/burger-removebg-preview.png', 'Beef Burger', 20, 'Big juicy beef burger with cheese, lettuce, tomator, onions, and special saucer!',4.8),
+    Product('asset/images/pizza.png', 'Chesese Pizza', 32, 'Big juicy beef burger with cheese, lettuce, tomator, onions, and special saucer!',4.8),
+    Product('asset/images/steak-removebg-preview.png', 'Beef Steak', 48, 'Big juicy beef burger with cheese, lettuce, tomator, onions, and special saucer!',4.8),
+    Product('asset/images/cake-removebg-preview.png', 'Chocolate Cake', 12, 'Big juicy beef burger with cheese, lettuce, tomator, onions, and special saucer!',4.8),
+    Product('asset/images/fries.png', 'French Fries', 4, 'Big juicy beef burger with cheese, lettuce, tomator, onions, and special saucer!',4.8),
+  ];
+
+
   List<Map<String, String>> tabViewItems = [
     {
       'name':"All",
